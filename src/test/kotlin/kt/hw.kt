1326888f90d2928c6d93c1bff940eaf1dcde71aa
chen <chen1406736605@outlook.com>
2018-10-27 13:11:05 +0800

    1.修改工程配置文件 2.测试kotlin

---
+++ src/test/kotlin/kt/hw.kt
@@ -0,0 +1,16 @@
+package kt
+
+import org.junit.Test
+
+/**
+ * @author chen
+ * @date 2018/10/27 13:08
+ */
+
+class hw{
+
+    @Test
+    fun sayHello() {
+        println("hello kotlin.")
+    }
+}
