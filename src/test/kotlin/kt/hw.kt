a53bf3934d73df70abbf4326be8762d7c44a6336
chen <chen1406736605@outlook.com>
2018-10-28 09:32:13 +0800

    add

--- src/test/kotlin/kt/hw.kt
+++ src/test/kotlin/kt/hw.kt
@@ -1,6 +1,11 @@
 package kt
 
+import cn.hutool.core.date.DatePattern
+import cn.hutool.core.date.DateTime
+import cn.hutool.core.date.DateUtil
 import org.junit.Test
+import java.time.ZoneId
+import java.util.*
 
 /**
  * @author chen
@@ -11,6 +16,8 @@ class hw{
 
     @Test
     fun sayHello() {
-        println("hello kotlin.")
+
+        var time = DateTime("2018-10-27 17:08:10", DatePattern.NORM_DATETIME_PATTERN)
+        println(DateTime.of(time))
     }
 }
